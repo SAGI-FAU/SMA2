d4a91f7890adbca4487372fc2a5769e0c4e05554
jcvasquezc <juancamilo0628@gmail.com>
2018-08-17 03:59:05 -0500

    first commit

---
+++ app/src/main/java/com/sma2/sma2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sma2.sma2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
