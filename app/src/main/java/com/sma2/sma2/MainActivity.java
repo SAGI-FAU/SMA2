4eceeb3d382361c3168b12697793d50e221ddaaf
PKlumpp <Fick7!Dich>
2018-10-05 13:25:05 +0200

    Splash Screen Implementation #1

--- app/src/main/java/com/sma2/sma2/MainActivity.java
+++ app/src/main/java/com/sma2/sma2/MainActivity.java
@@ -3,8 +3,6 @@ package com.sma2.sma2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
