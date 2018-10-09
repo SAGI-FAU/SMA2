54339b9c1c0c09e06291fd361ce2ef4e151cdd13
jcvasquezc <juancamilo0628@gmail.com>
2018-10-09 11:32:47 -0500

    capture speech pipeline ask for permission in main app

--- app/src/main/java/com/sma2/sma2/MainActivity.java
+++ app/src/main/java/com/sma2/sma2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.sma2.sma2;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    private int MY_PERMISSIONS_REQUEST_RECORD_AUDIO, MY_PERMISSIONS_REQUEST_WRITE_STORAGE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //setContentView(R.layout.activity_main_menu);
         setContentView(R.layout.activity_main_menu);
         setListeners();
+
+        ask_permissions();
+
+
 
     }
 
@@ -29,12 +37,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.txtSettings).setOnClickListener(this);
         findViewById(R.id.txtExercises).setOnClickListener(this);
         findViewById(R.id.txtResults).setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View view) {
-
         switch (view.getId()){
             case R.id.btnProfile:
             case R.id.txtProfile:
@@ -68,5 +74,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startActivity(intent_exercises);
     }
 
+    public void ask_permissions(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+   }
+        }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+            }
+        }
+
+
+    }
 
 }
