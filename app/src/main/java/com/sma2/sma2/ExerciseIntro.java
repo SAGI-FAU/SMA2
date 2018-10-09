4cf6195eabc6fd884c567b237a52d6bf3dcd9366
Arne Küderle <a.kuederle@gmail.com>
2018-10-09 14:28:15 -0500

    Implemented concept of Exercise intro fragment

---
+++ app/src/main/java/com/sma2/sma2/ExerciseIntro.java
@@ -0,0 +1,81 @@
+package com.sma2.sma2;
+
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+public class ExerciseIntro extends Fragment {
+
+    private String mExerciseName;
+    private Uri mVideoPath;
+    private Uri mInstructionPath;
+
+    OnStartClickedListener mStartClickedCallback;
+
+
+    public static ExerciseIntro newInstance(String exerciseName, Uri videoPath, Uri instructionPath) {
+        ExerciseIntro fragment = new ExerciseIntro();
+        Bundle args = new Bundle();
+        args.putString("NAME", exerciseName);
+        args.putString("VIDEO_PATH", videoPath.toString());
+        args.putString("INSTRUCTION_PATH", instructionPath.toString());
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public interface OnStartClickedListener {
+        void onStartClicked();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mExerciseName = getArguments().getString("NAME");
+            mVideoPath = Uri.parse(getArguments().getString("VIDEO_PATH"));
+            mInstructionPath = Uri.parse(getArguments().getString("INSTRUCTION_PATH"));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_exercise_intro, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Log.d("TEST", mExerciseName);
+        Log.d("TEST", mInstructionPath.toString());
+
+        // Set Title based on Intend Information
+        TextView exerciseTitle = view.findViewById(R.id.exerciseTitle);
+        exerciseTitle.setText(mExerciseName);
+
+        // Set Video based on Intend Information
+        if (mInstructionPath.toString() != null) {
+            VideoView instructionVideo = view.findViewById(R.id.instructionVideo);
+            instructionVideo.setVideoURI(mVideoPath);
+        } else {
+            // Throw runtime error
+        }
+
+        // Set On Click handler for Start Button based on Intent
+        Button startButton = view.findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStartClickedCallback.onStartClicked();
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
