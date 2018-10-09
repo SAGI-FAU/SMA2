8fea413020e3b18de9e4d279d77bd846e5f51ebf
Arne Küderle <a.kuederle@gmail.com>
2018-10-09 18:10:49 -0500

    Added dummy implimentation with the Vowel exercise and no DB

--- app/src/main/java/com/sma2/sma2/ExerciseSessionManager.java
+++ app/src/main/java/com/sma2/sma2/ExerciseSessionManager.java
@@ -1,15 +1,37 @@
 package com.sma2.sma2;
 
+import android.net.Uri;
+
+import com.sma2.sma2.ExerciseFragments.ExSustainedVowel;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseSessionManager {
+
+    private List<Exercise> _dummyExerciseList = new ArrayList<Exercise>();
     public void createExerciseSession() {
         // Create a new list of exercise and store them in the database with a new (incrementing) session id
         // store the current session id in the as a shared property
     }
 
-    public ArrayList<ScheduledExercise> getExerciseSession() {
-        // get the exercise list for the current session id
-        return new ArrayList<ScheduledExercise>();
+    public List<ScheduledExercise> getExerciseSession() {
+        // create Dummy List
+        _createDummyExerciseList();
+        List<ScheduledExercise> testList = new ArrayList<ScheduledExercise>();
+        testList.add(new ScheduledExercise(_dummyExerciseList.get(0), 1));
+        testList.add(new ScheduledExercise(_dummyExerciseList.get(0), 1));
+        testList.add(new ScheduledExercise(_dummyExerciseList.get(0), 1));
+        return testList;
+    }
+
+    public ScheduledExercise getNextExercise() {
+        return getExerciseSession().get(0);
+    }
+
+    public void _createDummyExerciseList(){
+        _dummyExerciseList.add(new Exercise("Test1", Uri.parse("video/path"), Uri.parse("Instruction/Path"), ExSustainedVowel.class));
+        _dummyExerciseList.add(new Exercise("Test2", Uri.parse("video/path"), Uri.parse("Instruction/Path"), ExSustainedVowel.class));
+        _dummyExerciseList.add(new Exercise("Test3", Uri.parse("video/path"), Uri.parse("Instruction/Path"), ExSustainedVowel.class));
     }
 }
