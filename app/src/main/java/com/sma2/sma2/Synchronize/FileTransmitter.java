3ab2dddd8780781589b74be1af1aac68ad740bd0
Martin Strauss <maddin.strauss@web.de>
2018-10-13 19:27:58 +0200

    Upload Service works

--- app/src/main/java/com/sma2/sma2/Synchronize/FileTransmitter.java
+++ app/src/main/java/com/sma2/sma2/Synchronize/FileTransmitter.java
@@ -8,38 +8,54 @@ import okhttp3.OkHttpClient;
 
 public class FileTransmitter {
 
+    // CAUTION: this is hard-coded for testing purposes. It needs to be adapted to the real server values
     public static final String SERVICE_URL = "http:10.2.3.0";//"http://localhost";
     public static final String FILE_SERVER_ROOT="http:10.2.3.0";//"http://localhost";
     public static final String LIBRARY="My Library"; // Find a name for the storage
 
-    //private static OkHttpClient client = new OkHttpClient();
-    OkHttpClient client = new OkHttpClient.Builder()
+    private OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(20, TimeUnit.SECONDS)
             .writeTimeout(20, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .build();
 
-
-
     private static SeafileApi api = new SeafileApi(SERVICE_URL,FILE_SERVER_ROOT);
 
+    // Username and password from the seafile server
     private String username;
     private String password;
     private String token;
 
-    public FileTransmitter(String username, String password) {
+    // Path variables for the seafile server
+    private String rel_Path = "";
+    private String parent_dir = "/";
+
+    public FileTransmitter(String username, String password) throws IllegalArgumentException{
         this.username = username;
         this.password = password;
+        // Get the authentication token for the seafile server
         this.token = api.obtainAuthToken(client, this.username, this.password);
+        if (this.token == null) {
+            throw new IllegalArgumentException("Obtaining the token did not work, check the username, " +
+                    "password or client specifications");
+        }
     }
 
-
-    public void uploadFile(File file) throws NullPointerException{
+    /**
+     * Method to upload the file. Needs to list all libraries first to get the repo_id for the
+     * upload link. Then it uses the upload link and token to upload the file
+     *
+     * @param file
+     *
+     * @return
+     */
+    public void uploadFile(File file) throws IllegalArgumentException{
 
         // Variables to store the directory id and the upload link we need to upload
         String repo_id = null;
         String uploadLink = null;
 
+        // List all existing libraries on the server
         List<jsonObject.Library> libraries = api.listLibraries(client,this.token);
 
         // Check the list of libraries, and save the repository id if it exists.
@@ -57,9 +73,12 @@ public class FileTransmitter {
                 uploadLink = uploadLink.replace("seafile.example.com","10.2.3.0");
             }
         } else {
-            throw new NullPointerException("The target repository was not found!");
+            throw new IllegalArgumentException("The target repository was not found!");
         }
         // Upload file
-        List<jsonObject.UploadFileRes> uploadFileResList=api.uploadFile(client,token,uploadLink,"/","",file);
+        List<jsonObject.UploadFileRes> uploadedFile = api.uploadFile(client,token,uploadLink,parent_dir,rel_Path,file);
+        if (uploadedFile == null) {
+            throw new IllegalArgumentException("Upload failed!");
+        }
     }
 }
--- app/src/main/java/com/sma2/sma2/Synchronize/UploadService.java
+++ app/src/main/java/com/sma2/sma2/Synchronize/UploadService.java
@@ -17,6 +17,10 @@ public class UploadService{
     private String password;
     private File file;
 
+    // In MILLISECONDS when to try to check for WIFI and upload again
+    // one hour 3600000
+    private int repeatTimer = 1800000; // Currently every half an hour
+
     Timer timer = new Timer();
 
     public UploadService(){}
@@ -28,65 +32,67 @@ public class UploadService{
         this.context = context;
     }
 
+    /**
+     * Method to time the upload service. Every "repeatTimer" it checks if there is a WIFI
+     * connection and tries to upload
+     *
+     * @param
+     *
+     * @return
+     */
     public void doTimedUpload() {
 
         timer.scheduleAtFixedRate(new TimerTask() {
 
           @Override
           public void run() {
-
               if (isOnline(context)) { // if there is a wifi connection
-
-                  Log.e("Online before upload","Check it out");
+                  Log.d("Online before upload","Check it out");
                   // Upload the file
-                  //performUpload(username, password, file);
-                  FileTransmitter transmitFile = new FileTransmitter(username, password);
-                  Log.e("Transmitter","new  Transmitter created");
-                  Log.e("File check",file.getAbsolutePath());
-                  transmitFile.uploadFile(file);
-                  Log.e("Transmitter","File was uploaded");
-
+                  performUpload(username,password,file);
                   // After upload cancel the timer
                   timer.cancel();
+                  return;
+              } else {
+                  Log.d("NoWIFI", "Wifi not established");
               }
-
-              Log.e("NoWIFI","Wifi not established");
           }
-
       },
-        0, 1000); //try again in one hour3600000
+        0, repeatTimer);
     }
 
-
+    /**
+     * Method to perform the file upload
+     *
+     * @param username
+     * @param password
+     *
+     * @return
+     */
     public void performUpload(String username, String password, File file) {
 
+        // Create a transmitFile Object
         FileTransmitter transmitFile = new FileTransmitter(username, password);
-        Log.e("Transmitter","new  Transmitter created");
+        Log.d("Transmitter","new  Transmitter created");
+        // Upload the file
         transmitFile.uploadFile(file);
-        Log.e("Transmitter","File was uploaded");
+        Log.d("Transmitter","File was uploaded");
     }
 
+    /**
+     * Method to check if there is a existing WIFI connection
+     *
+     * @param context
+     *
+     * @return true if WIFI connection established, false otherwise
+     */
     public boolean isOnline(Context context) {
 
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
 
-        Log.e("Online check",netInfo.getTypeName());
-        //should check null because in airplane mode it will be null
-        return (netInfo != null && netInfo.isConnected() && netInfo.getTypeName().equalsIgnoreCase("WIFI")); // MAKE SURE THAT ONLY WIFI IS POSSIBLE
+        //should check null because in airplane mode it will be null and make sure only WIFI
+        // connnection counts
+        return (netInfo != null && netInfo.isConnected()&& netInfo.getTypeName().equalsIgnoreCase("WIFI"));
     }
-
-    /*
-    private boolean haveNetworkConnection() {
-        boolean haveConnectedWifi = false;
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo ni : netInfo) {
-            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-                if (ni.isConnected())
-                    haveConnectedWifi = true;
-        }
-        return haveConnectedWifi;
-    }
-    */
 }
