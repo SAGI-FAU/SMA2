3628a48347c5f64ae969ede435e87f6798390c46
jcvasquezc <juancamilo0628@gmail.com>
2019-09-23 03:58:44 -0500

    change scheduler

--- app/src/main/java/com/sma2/sma2/ActivityChangeScheduler.java
+++ app/src/main/java/com/sma2/sma2/ActivityChangeScheduler.java
@@ -15,6 +15,13 @@ public class ActivityChangeScheduler extends AppCompatActivity implements View.O
 
     RadioGroup rg_exercise;
     SharedPreferences sharedPref;
+
+    String daily;
+    String full;
+    String speech;
+    String movement;
+    String demo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +29,13 @@ public class ActivityChangeScheduler extends AppCompatActivity implements View.O
         setContentView(R.layout.activity_change_scheduler);
         findViewById(R.id.button_next_scheduler).setOnClickListener(this);
         rg_exercise = findViewById(R.id.scheduler_radio);
+
+        daily=getResources().getString(R.string.daily_exercises);
+        full=getResources().getString(R.string.full_exercises);
+        speech=getResources().getString(R.string.speech_exercises);
+        movement=getResources().getString(R.string.mov_exercises);
+        demo=getResources().getString(R.string.demo_exercises);
     }
-
-
-
 
 
     @Override
@@ -34,17 +44,29 @@ public class ActivityChangeScheduler extends AppCompatActivity implements View.O
             case R.id.button_next_scheduler:
                 String exercises = ((RadioButton) findViewById(rg_exercise.getCheckedRadioButtonId())).getText().toString();
 
-                String daily=getResources().getString(R.string.daily_exercises);
-                String full=getResources().getString(R.string.full_exercises);
                 sharedPref =PreferenceManager.getDefaultSharedPreferences(this);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.apply();
+
+
                 if (exercises.equals(daily)){
                     editor.putString("exercises", "daily");
                     editor.apply();
                 }
-                else{
+                else if (exercises.equals(full)){
                     editor.putString("exercises", "full");
+                    editor.apply();
+                }
+                else if (exercises.equals(speech)){
+                    editor.putString("exercises", "speech");
+                    editor.apply();
+                }
+                else if (exercises.equals(movement)){
+                    editor.putString("exercises", "movement");
+                    editor.apply();
+                }
+                else{
+                    editor.putString("exercises", "demo");
                     editor.apply();
                 }
                 Intent intent_main =new Intent(this, MainActivityMenu.class);
