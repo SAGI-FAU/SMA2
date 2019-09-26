6715c048507726f86cffd0945c242519d4cae712
TOMAS <tomas9106@gmail.com>
2019-09-26 16:47:52 +0200

    Intonation and voice rate features included

--- app/src/main/java/com/sma2/sma2/DataAccess/ExerciseSessionManager.java
+++ app/src/main/java/com/sma2/sma2/DataAccess/ExerciseSessionManager.java
@@ -96,8 +96,7 @@ public class ExerciseSessionManager {
             }
         }
         else if (type_exercise.equals("full")){
-            //exercisesIDs = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38};
-            exercisesIDs = new int[]{11};
+            exercisesIDs = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38};
         }
         else if (type_exercise.equals("speech")){
             exercisesIDs = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
